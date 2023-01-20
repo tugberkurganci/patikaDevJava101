5b8727e70a6b3879cc0d89affd20e5d785fe4dac
Tuğberk Urgancı <tugberkurganci@gmail.com>
2023-01-20 19:06:08 +0300

    committed

---
+++ CircleZoneArea.java
@@ -0,0 +1,13 @@
+public class CircleZoneArea {
+
+    public void area(int radius,int angle){
+
+        double pi=3.14;
+
+        double area=(pi*(radius*radius)*angle)/360;
+
+        System.out.println(area);
+
+
+    }
+}
