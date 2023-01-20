e37bb78d0dd1bf5a8f4e276a0c85f9db1013a8f2
Tuğberk Urgancı <tugberkurganci@gmail.com>
2023-01-20 17:28:53 +0300

    committed

---
+++ TaxiFare.java
@@ -0,0 +1,11 @@
+public class TaxiFare {
+
+    public void cost(double kilometres){
+
+        double resultCost=10+(kilometres*2.20);
+
+        if (resultCost<20) resultCost=20;
+
+        System.out.println(resultCost);
+    }
+}
