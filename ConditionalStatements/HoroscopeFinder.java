e3f056327165a4a8d9417c277bd360dcf428bbe7
Tuğberk Urgancı <tugberkurganci@gmail.com>
2023-01-21 16:21:59 +0300

    committed

--- ConditionalStatements/HoroscopeFinder.java
+++ ConditionalStatements/HoroscopeFinder.java
@@ -1,55 +1,55 @@
 package ConditionalStatements;
 
 public class HoroscopeFinder {
-    public void find(int month,int day){
+    public void find(int month, int day) {
 
-        if (month==1){
-            if (day<22) System.out.println("oğlak");
-            else System.out.println("kova");
+        if (month == 1) {
+            if (day < 22) System.out.println("Your horoscope is:"+"capricorn");
+            else System.out.println("Your horoscope is:"+"aquarius");
         }
-        if (month==2){
-            if (day<20) System.out.println("kova");
-            else System.out.println("balık");
+        if (month == 2) {
+            if (day < 20) System.out.println("Your horoscope is:"+"aquarius");
+            else System.out.println("Your horoscope is:"+"pisces");
         }
-        if (month==3){
-            if (day<21) System.out.println("balık");
-            else System.out.println("koç");
+        if (month == 3) {
+            if (day < 21) System.out.println("Your horoscope is:"+"pisces");
+            else System.out.println("Your horoscope is:"+"aries");
         }
-        if (month==4){
-            if (day<21) System.out.println("koç");
-            else System.out.println("boğa");
+        if (month == 4) {
+            if (day < 21) System.out.println("Your horoscope is:"+"aries");
+            else System.out.println("Your horoscope is:"+"taurus");
         }
-        if (month==5){
-            if (day<22) System.out.println("boğa");
-            else System.out.println("ikizler");
+        if (month == 5) {
+            if (day < 22) System.out.println("Your horoscope is:"+"taurus");
+            else System.out.println("Your horoscope is:"+"gemini");
         }
-        if (month==6){
-            if (day<23) System.out.println("ikizler");
-            else System.out.println("yengeç");
+        if (month == 6) {
+            if (day < 23) System.out.println("Your horoscope is:"+"gemini");
+            else System.out.println("Your horoscope is:"+"cancer");
         }
-        if (month==7){
-            if (day<23) System.out.println("yengeç");
-            else System.out.println("aslan");
+        if (month == 7) {
+            if (day < 23) System.out.println("Your horoscope is:"+"cancer");
+            else System.out.println("Your horoscope is:"+"leo");
         }
-        if (month==8){
-            if (day<24) System.out.println("aslan");
-            else System.out.println("başak");
+        if (month == 8) {
+            if (day < 24) System.out.println("Your horoscope is:"+"leo");
+            else System.out.println("Your horoscope is:"+"virgo");
         }
-        if (month==9){
-            if (day<24) System.out.println("başak");
-            else System.out.println("terazi");
+        if (month == 9) {
+            if (day < 24) System.out.println("Your horoscope is:"+"virgo");
+            else System.out.println("Your horoscope is:"+"scales");
         }
-        if (month==10){
-            if (day<24) System.out.println("terazi");
-            else System.out.println("akrep");
+        if (month == 10) {
+            if (day < 24) System.out.println("Your horoscope is:"+"scales");
+            else System.out.println("Your horoscope is:"+"scorpio");
         }
-        if (month==11){
-            if (day<24) System.out.println("akrep");
-            else System.out.println("yay");
+        if (month == 11) {
+            if (day < 24) System.out.println("Your horoscope is:"+"scorpio");
+            else System.out.println("Your horoscope is:"+"sagittarius");
         }
-        if (month==12){
-            if (day<23) System.out.println("yay");
-            else System.out.println("oğlak");
+        if (month == 12) {
+            if (day < 23) System.out.println("Your horoscope is:"+"sagittarius");
+            else System.out.println("Your horoscope is:"+"capricorn");
         }
 
     }
