2c449fd54562dba5c2d415c826874d179c309c94
Tuğberk Urgancı <tugberkurganci@gmail.com>
2023-01-22 17:36:45 +0300

    committed

---
+++ Loops/AverageOfDivisorsBy12.java
@@ -0,0 +1,22 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class AverageOfDivisorsBy12 {
+    public void aodsb12(){
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number=scanner.nextInt();
+        int total=0;
+        int count=0;
+        for (int i = 1; i <number ; i++) {
+            if (i%3==0 && i%4==0){
+                total+=i;
+                count++;
+
+            }
+        }
+        System.out.println("Average is: "+total/count);
+    }
+}
