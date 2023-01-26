c36964a2034db4c89c031f4b4aca3ffea12b007a
Tuğberk Urgancı <tugberkurganci@gmail.com>
2023-01-26 14:05:58 +0300

    committed

--- Series/MaxMinFinder.java
+++ Series/MaxMinFinder.java
@@ -2,8 +2,39 @@ package Series;
 
 import java.util.Arrays;
 
-public class MaxMinFinder {
-    public static void finder(int[] arr, int number1) {
+public class MaxMinFinder { //1,2,3,4
+    public static void finder1(int[] arr, int number1) {
+
+        Arrays.sort(arr);
+
+        int smallNearestNumber = arr[0];
+
+        int bigNearestNumber = arr[arr.length - 1];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i]<number1)smallNearestNumber=arr[i];else break;
+        }
+        for (int i = arr.length-1; i >0; i--) {
+
+            if (arr[i]>number1)bigNearestNumber=arr[i];else break;
+        }
+        if(number1>bigNearestNumber){
+            System.out.println("smallestest number :"+smallNearestNumber);
+            System.out.println("this number biggest");
+            return;
+        }
+        if (number1<smallNearestNumber){
+            System.out.println("this number smallest");
+            System.out.println("biggest number :"+bigNearestNumber);
+            return;
+        }
+        System.out.println(smallNearestNumber);
+        System.out.println(bigNearestNumber);
+    }
+
+    public static void finder2(int[] arr, int number1) {
+
 
         int[] newArr = Arrays.copyOf(arr, arr.length + 1);
 
